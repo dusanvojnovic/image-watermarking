724a64e6b7bf000951a153f93ca606ed10c37b7d
Your Name <you@example.com>
2021-03-01 12:54:54 +0100

    resizing

--- watermark.py
+++ watermark.py
@@ -14,7 +14,7 @@ def submit():
 def open_img():
     global canvas
     img = Image.open(filedialog.askopenfilename(initialdir = "/",title = "Select A File",filetypes = (("jpeg files","*.jpg"),("all files","*.*"))))
-    img = img.resize((800, 525), Image.ANTIALIAS)
+    img = img.resize((1250, 700), Image.ANTIALIAS)
     photo = ImageTk.PhotoImage(img)
     text = text_var.get()
     canvas.image = photo
@@ -29,29 +29,29 @@ def save_img():
     y1=y+canvas.winfo_height()
     box=(x,y,x1,y1)
     grabcanvas = ImageGrab.grab().crop(box)
-    grabcanvas.save(fp=filedialog.asksaveasfilename(initialdir = "/",title = "Select A File",filetypes = (("jpeg files","*.jpg"),("all files","*.*")),  defaultextension ='*.jpg'))
+    grabcanvas.save(fp=filedialog.asksaveasfilename(initialdir = "/",title = "Save Image",filetypes = (("jpeg files","*.jpg"),("all files","*.*")),  defaultextension ='*.jpg'))
 
 window = Tk()
 window.title("WatermarkImg")
-window.minsize(width = 980, height = 700)
+window.minsize(width = 1400, height = 900)
 window.config(padx=70, pady=15, bg="#a1cae2")
 
 text_var = StringVar()
 
-canvas = Canvas(width = 800, height = 500)
+canvas = Canvas(width = 1250, height = 700)
 watermark_label = Label(text = "Add watermark text here: ")
 watermark_label.config(font = 30)
-watermark_label.place(x=70, y=40)
+watermark_label.place(x=220, y=40)
 watermark_label.config(bg="#a1cae2")
 watermark_entry = Entry(width = 60, textvariable = text_var)
-watermark_entry.place(x=270, y =35, width= 270, height = 30)
+watermark_entry.place(x=460, y =35, width= 270, height = 30)
 
 submit = Button(text = "Submit", command = submit, width=20, height=1)
-submit.place(x=580, y = 37)
+submit.place(x=880, y = 37)
 add_file = Button(text="Select Image", command = open_img, width=15, height=1)
-add_file.place(x=200, y=100)
+add_file.place(x=300, y=100)
 save_file = Button(text = "Save Image", command = save_img, width=15, height=1)
-save_file.place(x=500, y=100)
+save_file.place(x=750, y=100)
 
 
 window.mainloop()
